3d516d1b684259da596c3a4ed99e06c6514a38b6
iantreza19 <55836534+iantreza19@users.noreply.github.com>
2019-09-26 20:02:19 +0700

    Add files via upload

---
+++ Latihan1/src/PBOUlang10118802Latihan1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author IantReza
+ */
+public class PBOUlang10118802Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR"+"\n"+"---------------------");
+        System.out.println("NAMA\t\t: Iant Reza Anshory");
+        System.out.println("ALAMAT\t\t: Perum Bintang Alam Blok A1 no.3, Karawang");
+        System.out.println("TEMPAT LAHIR\t: Brebes");
+        System.out.println("TANGGAL LAHIR\t: 19 Januari 1994");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t\t: Nonton Film");
+        System.out.println("MAKES\t\t: Rendang");
+        System.out.println("MIKES\t\t: Es Teh");
+    }
+    
+}
